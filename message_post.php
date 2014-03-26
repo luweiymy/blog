48305b6862ed31d1c332d4ef7e26ef3717f6e569
luweiymy <1289375156@qq.com>
2014-03-26 22:50:37 +0800

    clear merge

--- message_post.php
+++
@@ -1,9 +0,0 @@
-<?php//检查表单输入
-	header('Content-Type:text/xml');
-	require "message.php";
-	session_start();
-	$id=$_POST['id'];
-	$name=$_POST['name'];
-	$contents=$_POST['contents'];
-
-?>
--- severtime.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header('Content-Type:text/xml');
-	echo "<div>
-	<p>it  is ".date('H:i:s')."on".date("M d,Y")."</p>
-	</div>
-	"
-?>
