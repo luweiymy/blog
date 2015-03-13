e55da11cb79bf15154e6ed837b9ee307e78bac15
luweiymy <luweiymy@users.noreply.github.com>
2015-03-13 17:50:23 +0800

    修改了点东西

--- data/message.php
+++ data/message.php
@@ -1,10 +1,13 @@
 <?php  //向数据库中插入数据
-	header("Content-type:text/html;charset=utf-8");
-	require "key.php";
-	$id=$_POST['id'];
-	$name=$_POST['name'];
-	$contents=$_POST['contents'];
-	$time = date('Y-m-d H:i:s',time());//留言的时间
+	// header("Content-type:text/html;charset=utf-8");
+	 require "key.php";
+	// // $id=$_POST['id'];
+	// $name=$_POST['name'];
+	// $contents=$_POST['contents'];
+	// $time = date('Y-m-d H:i:s',time());//留言的时间
+	//使用ajax传过来的数据
+	$data = json_decode($_POST['data'],true);
+	print_r($data['usernames']);
 	$conn=mysql_connect($host,$user,$password)or die("连接数据库失败".mysql_error());
 	mysql_select_db("blog");
 	$query="insert into message (name,contents,time) values('".$name."','".$contents."','".$time."')";
